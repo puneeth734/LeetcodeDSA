bf24c02dadc0587bf85f301ffc807d8a422960ce
Puneeth N <puneethna87@gmail.com>
2026-09-23 12:47:30 +0530

    Time: 3 ms (87.35%), Space: 52.5 MB (21.46%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,48 +1,30 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) return 0;
-
-        int rows = grid.length;
-        int cols = grid[0].length;
-        int islands = 0;
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (grid[i][j] == '1') {
-                    bfs(grid, i, j);
-                    islands++;
+        int a=grid.length;
+        int b=grid[0].length;
+        int cout=0;
+        for(int i=0;i<a;i++)
+        {
+            for(int j=0;j<b;j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    cout++;
+                    dfs(grid,i,j,a,b);
                 }
             }
         }
-
-        return islands;
+        return cout;
     }
-
-    private void bfs(char[][] grid, int r, int c) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{r, c});
-        grid[r][c] = '0'; // Mark as visited
-
-        int[] dr = {-1, 1, 0, 0};
-        int[] dc = {0, 0, -1, 1};
-
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int currR = current[0];
-            int currC = current[1];
-
-            for (int i = 0; i < 4; i++) {
-                int newR = currR + dr[i];
-                int newC = currC + dc[i];
-
-                if (newR >= 0 && newR < rows && newC >= 0 && newC < cols && grid[newR][newC] == '1') {
-                    queue.add(new int[]{newR, newC});
-                    grid[newR][newC] = '0'; // Mark as visited
-                }
-            }
+    void dfs(char [][] arr,int i,int j,int a,int b)
+    {
+        if(i>=a || j>=b || i<0 || j<0 || arr[i][j]=='0'){
+            return;
         }
+        arr[i][j] = '0';
+        dfs(arr,i+1,j,a,b);
+        dfs(arr,i-1,j,a,b);
+        dfs(arr,i,j+1,a,b);
+        dfs(arr,i,j-1,a,b);
     }
 }
